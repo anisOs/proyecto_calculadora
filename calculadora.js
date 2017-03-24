61048420d426095f302fd4e395ed3e04a3e456e7
AnisOs <anisccc@gmail.com>
2017-03-24 11:55:16 -0600

    nueva version

--- calculadora.js
+++ calculadora.js
@@ -1,75 +1,75 @@
 window.onload = function(){
-pantalla=document.getElementById("pantallita");
+pantalla = document.getElementById("pantallita");
 }
-numPantalla="0";
-xi=1;
-decimal=0;
-ni=0;
-op="no";
+var numPantalla = "0";
+var iniPantalla = 1;
+var decimal = 0;
+var ni = 0;
+var op = "no";
 
 function numero(num) {
-         if (numPantalla=="0" || xi==1  ) {
-            pantalla.innerHTML=num;
-            numPantalla=num;
-            if (num==".") {
-               pantalla.innerHTML="0.";
-               numPantalla=num;
-               decimal=1;
+         if (numPantalla == "0" || iniPantalla == 1  ) {
+            pantalla.innerHTML = num;
+            numPantalla = num;
+            if (num == ".") {
+               pantalla.innerHTML = "0.";
+               numPantalla = num;
+               decimal = 1;
                }
            }
            else {
-               if (num=="." && decimal==0) {
-                   pantalla.innerHTML+=num;
-                   numPantalla+=num;
-                   decimal=1;
+               if (num == "." && decimal == 0) {
+                   pantalla.innerHTML += num;
+                   numPantalla += num;
+                   decimal = 1;
                }
-               else if (num=="." && decimal==1) {}
+               else if (num == "." && decimal == 1) {}
                else {
-                   pantalla.innerHTML+=num;
-                   numPantalla+=num
+                   pantalla.innerHTML += num;
+                   numPantalla += num
                }
             }
-            xi=0
+            iniPantalla = 0
          }
 function operar(s) {
          igual()
-         ni=numPantalla
-         op=s;
-         xi=1;
+         ni = numPantalla
+         op = s;
+         iniPantalla = 1;
          }
 function igual() {
-         if (op=="no") {
-            pantalla.innerHTML= numPantalla;
+         if (op == "no") {
+            pantalla.innerHTML = numPantalla;
             }
          else {
-            sl=ni+op+numPantalla;
-            sol=eval(sl)
-            pantalla.innerHTML=sol
-            numPantalla=sol;
-            op="no";
-            xi=1;
+            sl = ni + op + numPantalla;
+            sol = eval(sl)
+            pantalla.innerHTML = sol
+            numPantalla = sol;
+            op = "no";
+            iniPantalla = 1;
             }
         }
 function raizcuadrada() {
-         numPantalla=Math.sqrt(numPantalla)
-         pantalla.innerHTML=numPantalla;
-         op="no";
-         xi=1;
+         numPantalla = Math.sqrt(numPantalla)
+         pantalla.innerHTML = numPantalla;
+         op = "no";
+         iniPantalla = 1;
          }
 function porcentaje() {
-         numPantalla=numPantalla/100
-         pantalla.innerHTML=numPantalla;
+         numPantalla = numPantalla/100
+         pantalla.innerHTML = numPantalla;
          igualar()
-         xi=1
+         iniPantalla = 1
          }
 function opuesto() {
-         nx=Number(numPantalla);
-         nx=-nx;
-         numPantalla=String(nx);
-         pantalla.innerHTML=numPantalla;
+         nx = Number(numPantalla);
+         nx =- nx;
+         numPantalla = String(nx);
+         pantalla.innerHTML = numPantalla;
          }
 function exponente() {
-         numPantalla = Math.pow(numPantalla,2)
-         pantalla.innerHTML=numPantalla;
-         xi=1;
+         numPantalla = Math.pow( numPantalla)
+         pantalla.innerHTML = numPantalla;
+         iniPantalla = 1;
          }
